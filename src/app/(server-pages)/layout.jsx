dddf802ef5b8d91c84bff44c555b776306b4cc5c
DhrumilP2905 <122265490+dhrumilpanchalrooton@users.noreply.github.com>
2024-04-21 20:56:03 +0530

    Remover bar loader

--- src/app/(server-pages)/layout.jsx
+++ src/app/(server-pages)/layout.jsx
@@ -1,35 +1,10 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 // import Header from "@/components/Header";
 // import Footer from "@/components/Footer";
-import AOS from "aos";
-import "aos/dist/aos.css";
-// import { Suspense } from "react";
-import { BarLoader } from "react-spinners";
 
 export default function RootLayout({ children }) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-    AOS.init({
-      offset: 0,
-      duration: 1000,
-    });
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="h-screen w-full flex items-center justify-center">
-        <BarLoader color="#f6c400" height={4} />
-      </div>
-    );
-  }
-
   return (
     <>
       {/* <Header /> */}
@@ -38,4 +13,3 @@ export default function RootLayout({ children }) {
     </>
   );
 }
-
