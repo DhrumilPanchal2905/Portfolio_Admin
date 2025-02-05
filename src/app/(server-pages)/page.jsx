c791dd08dd5596dec2119f885cab40cad066590f
DhrumilP2905 <dhrumilpanchal295@gmail.com>
2025-02-06 01:28:57 +0530

    update grid

--- src/app/(server-pages)/page.jsx
+++ src/app/(server-pages)/page.jsx
@@ -15,27 +15,27 @@ import LoginModal from "@/components/LoginModal";
 import axios from "axios";
 // import { AuroraBackground } from "@/components/UI";
 
-export const metadata = {
-  title: metaInfo.home.title,
-  description: metaInfo.home.description,
-  alternates: { canonical: "https://dev.dhrumilpanchal.in" },
-  openGraph: {
-    url: `${process.env.NEXT_APP_BASE_URL}`,
-    title: metaInfo.home.title,
-    images: `${process.env.NEXT_APP_BASE_URL}/images/og-image.png`,
-    description: metaInfo.home.description,
-    type: "article",
-  },
-  twitter: {
-    title: "Form | Dhrumil Panchal",
-    description:
-      "Explore Dhrumil Panchal's portfolio showcasing MERN stack projects and web development expertise.",
-    card: "summary_large_image",
-  },
-  robots: {
-    index: process.env.NEXT_APP_ENVIRONMENT === "production",
-  },
-};
+// export const metadata = {
+//   title: metaInfo.home.title,
+//   description: metaInfo.home.description,
+//   alternates: { canonical: "https://dev.dhrumilpanchal.in" },
+//   openGraph: {
+//     url: `${process.env.NEXT_APP_BASE_URL}`,
+//     title: metaInfo.home.title,
+//     images: `${process.env.NEXT_APP_BASE_URL}/images/og-image.png`,
+//     description: metaInfo.home.description,
+//     type: "article",
+//   },
+//   twitter: {
+//     title: "Form | Dhrumil Panchal",
+//     description:
+//       "Explore Dhrumil Panchal's portfolio showcasing MERN stack projects and web development expertise.",
+//     card: "summary_large_image",
+//   },
+//   robots: {
+//     index: process.env.NEXT_APP_ENVIRONMENT === "production",
+//   },
+// };
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
